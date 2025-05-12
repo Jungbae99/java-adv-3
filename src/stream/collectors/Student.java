c126306468e6b8fa992178edec6fcb72a4766fd1
전정배 <116333199+Jungbae99@users.noreply.github.com>
2025-05-13 00:17:29 +0900

    10.2 DownStreamCollector 1

---
+++ src/stream/collectors/Student.java
@@ -0,0 +1,47 @@
+package stream.collectors;
+
+public class Student {
+    private String name;
+    private int grade;
+    private int score;
+
+    public Student(String name, int grade, int score) {
+        this.name = name;
+        this.grade = grade;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", grade=" + grade +
+                ", score=" + score +
+                '}';
+    }
+}
