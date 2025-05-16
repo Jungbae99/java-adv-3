e98b7f354bbf25366f16c05252aca4eb30715611
전정배 <wjdqotktls55@gmail.com>
2025-05-16 15:53:29 +0900

    13.4 자바 병렬스트림과 주의점2
    - I/O 바운드에서 쓰지말자
    - 로직풀을 사용하자

---
+++ src/parallel/ParallelMain7.java
@@ -0,0 +1,49 @@
+package parallel;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.IntStream;
+
+import static util.MyLogger.log;
+
+public class ParallelMain7 {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService requestPool = Executors.newFixedThreadPool(100);
+
+        // 로직저리 전용 스레드풀 추가
+        ExecutorService logicPool = Executors.newFixedThreadPool(400);
+        int nThreads = 10; // 고객의 요청
+
+        for (int i = 1; i <= nThreads; i++) {
+            String requestName = "request" + i;
+            requestPool.submit(() -> logic(requestName, logicPool));
+            Thread.sleep(100); // 스레드 순서를 확인하기 위해 약간 대기
+        }
+        requestPool.close();
+        logicPool.close();
+    }
+
+    private static void logic(String requestName, ExecutorService es) {
+        log("[" + requestName + "] START");
+        long startTime = System.currentTimeMillis();
+
+        List<Future<Integer>> list = IntStream.rangeClosed(1, 4)
+                .mapToObj(i -> es.submit(() -> HeavyJob.heavyTask(i, requestName)))
+                .toList();
+
+        int sum = list.stream()
+                .mapToInt(f -> {
+                    try {
+                        return f.get();
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }).sum();
+
+        long endTime = System.currentTimeMillis();
+        log("[" + requestName + "] time: " + (endTime - startTime) + "ms, sum: " + sum);
+    }
+}
